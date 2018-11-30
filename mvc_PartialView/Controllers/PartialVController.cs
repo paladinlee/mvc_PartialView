77e13a8cb6376e21353f085d287dde4b96a226ce
paladinlee <paladinlee@gmail.com>
2018-11-30 15:11:28 +0800

    即便你知道 Partial View 的叫用網址，也不能在瀏覽器中直接打開

--- mvc_PartialView/Controllers/PartialVController.cs
+++ mvc_PartialView/Controllers/PartialVController.cs
@@ -19,8 +19,21 @@ namespace mvc_PartialView.Controllers
         {
             Thread.Sleep(2000);
 
-            //字首加底線、字尾加 Partial 是 PartialView 的命名規則
-            return PartialView("_GetTimePartial");
+            //限定同網站的Ajax專用
+            //也就是即便你知道 Partial View 的叫用網址，也不能在瀏覽器中直接打開
+            //Ref:https://www.evernote.com/shard/s307/nl/49104230/a819f692-da81-46f5-80fa-38352ee775a8
+            if (Request.IsAjaxRequest())
+            {
+                //字首加底線、字尾加 Partial 是 PartialView 的命名規則
+                return PartialView("_GetTimePartial");
+            }
+            else
+            {
+                return Content("Fail");
+            }
+
+
+
         }
     }
 }
